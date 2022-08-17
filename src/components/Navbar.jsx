4560c870434660f5e0042c10594f4503d8e61219
ngc.918 <nicolas.g9182@gmail.com>
2022-08-17 16:36:17 -0400

    more links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -109,7 +109,7 @@ const Navbar = () => {
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
 						<a
 							className="flex justify-between items-center w-full text-gray-300"
-							href="/"
+							href="https://drive.google.com/file/d/1J2aYyY6Ub-OokriF5qnHC9DgVTjpnBV9/view?usp=sharing"
 						>
 							Resume <BsFillPersonLinesFill size={30} />
 						</a>

bcab68a0dbe271549158ce332996ebdf8fff6169
ngc.918 <nicolas.g9182@gmail.com>
2022-11-08 11:59:15 -0500

    reorganized

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -112,6 +112,8 @@ const Navbar = () => {
 				<ul>
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
 						<a
+							target="_blank"
+							rel="noopener noreferrer"
 							className="flex justify-between items-center w-full text-gray-300"
 							href="https://www.linkedin.com/in/ngonz09/"
 						>
@@ -120,6 +122,8 @@ const Navbar = () => {
 					</li>
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
 						<a
+							target="_blank"
+							rel="noopener noreferrer"
 							className="flex justify-between items-center w-full text-gray-300"
 							href="https://github.com/ngc918"
 						>
@@ -128,6 +132,8 @@ const Navbar = () => {
 					</li>
 					<li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
 						<a
+							target="_blank"
+							rel="noopener noreferrer"
 							className="flex justify-between items-center w-full text-gray-300"
 							href="https://drive.google.com/file/d/1J2aYyY6Ub-OokriF5qnHC9DgVTjpnBV9/view?usp=sharing"
 						>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,57 +22,28 @@ const Work = () => {
 				<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
 					{/*Grid Item*/}
 					<div
-						style={{ backgroundImage: `url(${BrickBreak})` }}
+						style={{ backgroundImage: `url(${Fitness})` }}
 						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
 					>
 						{/*Hover Effect*/}
 						<div className="opacity-0 group-hover:opacity-100 text-center">
 							<span className="text-2xl font-bold text-white tracking-wider">
-								Brick Breaker
+								Angular Fitness
 							</span>
 							<div className="mt-3">
 								<span>
-									This project was built using HTML, CSS, and JS. Functions such
-									as object were included as a function, as well as lives.
+									Fitness tracking app with built in workouts. Built using
+									Angular, Angular Material, Firebase, and NGRX state
+									management.
 								</span>
 							</div>
 							<div className="pt-8 text-center">
-								<a href="https://ngc918.github.io/brick-breaker-project/">
+								<a href="https://angular-fitness-tracker-a6e8c.web.app/">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg">
 										Demo
 									</button>
 								</a>
-								<a href="https://github.com/ngc918/brick-breaker-project">
-									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg">
-										Code
-									</button>
-								</a>
-							</div>
-						</div>
-					</div>
-					<div
-						style={{ backgroundImage: `url(${GameRealms})` }}
-						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-					>
-						{/*Hover Effect*/}
-						<div className="opacity-0 group-hover:opacity-100 text-center">
-							<span className="text-2xl font-bold text-white tracking-wider">
-								Game Realms
-							</span>
-							<div className="mt-3">
-								<span>
-									A wishlist site for games was made using the IGDB API. Login
-									and signup function implemented, as well as a wishlist
-									function. Boostrap used for styling.
-								</span>
-							</div>
-							<div className="pt-8 text-center">
-								<a href="https://game-realms.herokuapp.com/">
-									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg">
-										Demo
-									</button>
-								</a>
-								<a href="https://github.com/ngc918/game-realms">
+								<a href="https://github.com/ngc918/angular-fitness-app">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg">
 										Code
 									</button>
@@ -141,28 +112,57 @@ const Work = () => {
 					</div>
 
 					<div
-						style={{ backgroundImage: `url(${Fitness})` }}
+						style={{ backgroundImage: `url(${GameRealms})` }}
 						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
 					>
 						{/*Hover Effect*/}
 						<div className="opacity-0 group-hover:opacity-100 text-center">
 							<span className="text-2xl font-bold text-white tracking-wider">
-								Angular Fitness
+								Game Realms
 							</span>
 							<div className="mt-3">
 								<span>
-									Fitness tracking app with built in workouts. Built using
-									Angular, Angular Material, Firebase, and NGRX state
-									management.
+									A wishlist site for games was made using the IGDB API. Login
+									and signup function implemented, as well as a wishlist
+									function. Boostrap used for styling.
 								</span>
 							</div>
 							<div className="pt-8 text-center">
-								<a href="https://angular-fitness-tracker-a6e8c.web.app/">
+								<a href="https://game-realms.herokuapp.com/">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg">
 										Demo
 									</button>
 								</a>
-								<a href="https://github.com/ngc918/angular-fitness-app">
+								<a href="https://github.com/ngc918/game-realms">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg">
+										Code
+									</button>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div
+						style={{ backgroundImage: `url(${BrickBreak})` }}
+						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+					>
+						{/*Hover Effect*/}
+						<div className="opacity-0 group-hover:opacity-100 text-center">
+							<span className="text-2xl font-bold text-white tracking-wider">
+								Brick Breaker
+							</span>
+							<div className="mt-3">
+								<span>
+									This project was built using HTML, CSS, and JS. Functions such
+									as object were included as a function, as well as lives.
+								</span>
+							</div>
+							<div className="pt-8 text-center">
+								<a href="https://ngc918.github.io/brick-breaker-project/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg">
+										Demo
+									</button>
+								</a>
+								<a href="https://github.com/ngc918/brick-breaker-project">
 									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 text-lg">
 										Code
 									</button>

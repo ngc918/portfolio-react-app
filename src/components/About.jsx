04476840b28d7e45477d7c90d8e1ae4c4dc837be
ngc.918 <nicolas.g9182@gmail.com>
2023-02-01 23:44:18 -0500

    changed about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,10 +12,7 @@ const About = () => {
 				</div>
 				<div className="w-max-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
 					<div className="sm:text-right text-4xl font-bold">
-						<p>
-							Hi, I'm Nicolas, nice to meet you. Please feel free to look
-							around.
-						</p>
+						<p>Please feel free to look around.</p>
 					</div>
 					<div>
 						<p>
